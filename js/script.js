befb894c7b580f081ee4a02e906a25f7b76e2cc0
moontasin <me@moontasin.com>
2020-12-19 11:51:05 +0600

    responsive done

---
+++ js/script.js
@@ -0,0 +1,177 @@
+// fixed/sticky menu
+
+$(window).scroll(function(){
+  var scrollamount = $(window).scrollTop()
+  
+  if(scrollamount > 500){
+    $(".menu").addClass("fixed")
+  }else{
+    $(".menu").removeClass("fixed")
+  }
+
+  if(scrollamount > 1000){
+    $(".btop").fadeIn();
+  }else{
+    $(".btop").fadeOut();
+  }
+})
+
+
+$(".btop").click(function(){
+  $("html,body").animate({
+    scrollTop:0
+  },1000)
+})
+
+
+
+// banner js
+$('#banner').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  autoplay: true,
+  autoSpeed: 1000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: '<i class="fa fa-angle-left prev" aria-hidden="true"></i>',
+  nextArrow: '<i class="fa fa-angle-right next" aria-hidden="true"></i>',
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+// ripples
+$('#banner .gym').ripples();
+$('#banner .one').ripples();
+$('#banner .two').ripples();
+
+
+// venbox js
+$('.venobox').venobox();
+
+
+// testi-slider js
+$('.testi-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoSpeed: 1000,
+  arrows: false,
+
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 767,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 576,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+
+});
+
+
+
+// counter js
+$('.counter').counterUp({
+  delay: 10,
+  time: 1000
+});
+
+// tab js
+$("#colorful").colorfulTab(); 
+
+
+// sponsor slider
+$('.sponsor-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoSpeed: 1000,
+  arrows: true,
+  prevArrow: '<i class="fa fa-angle-left prev" aria-hidden="true"></i>',
+  nextArrow: '<i class="fa fa-angle-right next" aria-hidden="true"></i>',
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 767,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 576,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+
+});
+
+
+ 
